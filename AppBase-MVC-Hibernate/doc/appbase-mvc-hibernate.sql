d48e28adc2dd676ac0d9556b23463be7486a8f93
van <van>
2017-01-06 17:41:52 +0800

    first commit

---
+++ AppBase-MVC-Hibernate/doc/appbase-mvc-hibernate.sql
@@ -0,0 +1,89 @@
+/*
+SQLyog Job Agent v11.11 (64 bit) Copyright(c) Webyog Inc. All Rights Reserved.
+
+
+MySQL - 5.6.5-m8 : Database - appbase_mvc_hibernate
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `app_user` */
+
+DROP TABLE IF EXISTS `app_user`;
+
+CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_expired` bit(1) NOT NULL,
+  `account_locked` bit(1) NOT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(15) DEFAULT NULL,
+  `province` varchar(100) DEFAULT NULL,
+  `credentials_expired` bit(1) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `account_enabled` bit(1) DEFAULT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `password_hint` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `version` int(11) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `app_user` */
+
+insert  into `app_user`(`id`,`account_expired`,`account_locked`,`address`,`city`,`country`,`postal_code`,`province`,`credentials_expired`,`email`,`account_enabled`,`first_name`,`last_name`,`password`,`password_hint`,`phone_number`,`username`,`version`,`website`) values (-2,'\0','\0','','Denver','US','80210','CO','\0','matt@raibledesigns.com','','Matt','Raible','a40546cc4fd6a12572828bb803380888ad1bfdab','Not a female kitty.','','admin',1,'http://raibledesigns.com');
+insert  into `app_user`(`id`,`account_expired`,`account_locked`,`address`,`city`,`country`,`postal_code`,`province`,`credentials_expired`,`email`,`account_enabled`,`first_name`,`last_name`,`password`,`password_hint`,`phone_number`,`username`,`version`,`website`) values (-1,'\0','\0','','Denver','US','80210','CO','\0','matt_raible@yahoo.com','','Tomcat','User','b6b1f4781776979c0775c71ebdd8bdc084aac5fe','A male kitty.','','user',1,'http://tomcat.apache.org');
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(64) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`description`,`name`) values (-2,'Default role for all Users','ROLE_USER');
+insert  into `role`(`id`,`description`,`name`) values (-1,'Administrator role (can edit Users)','ROLE_ADMIN');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FK143BF46A4FD90D75` (`role_id`),
+  KEY `FK143BF46AF503D155` (`user_id`),
+  CONSTRAINT `FK143BF46A4FD90D75` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FK143BF46AF503D155` FOREIGN KEY (`user_id`) REFERENCES `app_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`user_id`,`role_id`) values (-1,-2);
+insert  into `user_role`(`user_id`,`role_id`) values (-2,-1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ AppBase-MVC-Hibernate/src/main/java/com/app/demo/dao/SearchException.java
@@ -0,0 +1,13 @@
+package com.app.demo.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
